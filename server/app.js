2cc321f07a9603c029db1d3d4acb860282b7164c
Unkn0wn <unkn0wn9@outlook.com>
2023-02-10 17:47:39 +0800

    * add jwt token generate // unfinished

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
 
+const parameter = require('koa-parameter');
+const { koaBody } = require('koa-body');
+
+app.use(koaBody())
 app.use(cors())
 
 // 引入控制器
@@ -18,9 +22,15 @@ app.use(router.routes())
 // 允许Post
 app.use(router.allowedMethods())
 
+parameter(app)
+
 app.use(async (ctx) => {
     ctx.body = 'xChat Server Status: OK!'
 })
+
+app.on("error", (err, ctx) => {
+    console.error("Ooops..\n", err);
+});
 
 app.listen(3001, () => {
     console.log('localhost:3001')
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,10 +1,42 @@
 const Router = require('koa-router')
 const router = new Router()
-const koaBody = require('koa-body')
+
+const jwt = require('koa-jwt')
+const { sign } = require('jsonwebtoken');
+
+const secret = 'xChat'
+
+router.use(jwt({ secret }).unless({
+    path: [
+        '/users/login'
+    ]
+}))
 
 router.get('/', async (ctx, next) => {
     ctx.body = {
         data: 'Developing',
+        code: 200
+    }
+})
+
+router.post('/login', async (ctx, next) => {
+    ctx.verifyParams({
+        email: { type: 'email', required: true },
+        password: { type: 'string', required: true }
+    });
+
+    const params = ctx.request.body
+    const { email, password } = params
+
+    // TODO
+    if (email != 'xChat@qq.com' && password != 'xChat') {
+        ctx.throw(403, '用户名密码错误')
+    }
+    const token = sign({ email: email }, secret, { expiresIn: '1d' })
+    ctx.body = {
+        data: {
+            'token': token
+        },
         code: 200
     }
 })
